79bbb8cf37b53ef89d2addca330ca5e02b33dd99
tyler.maran <tyler.maran@gmail.com>
2018-09-20 12:43:12 +0700

    more moble responsive stuff!

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ var cards = {
         "https://dummyimage.com/400x400/000/fff",
         "https://dummyimage.com/400x400/000/fff",
         "https://dummyimage.com/300x300/000/fff"
+    ],
+    "text": [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     ]
-}
-
-// Ditto
-var content = {
-    "Day1": "Stuff goes here."
 }
 
 // Initializes page with country squares
@@ -53,7 +54,6 @@ function loadPage() {
         expand($(this).text());
     });
 
-
 }
 
 // Expands card to full page on click
@@ -78,7 +78,6 @@ function expand(name) {
     $(".card-grid").flip(false);
 
 
-
     // When flip is done, indiana jones the element and then animate the new guy
     $("#tile_" + name).on('flip:done', function () {
         $("#tile_" + name).hide();
@@ -89,9 +88,14 @@ function expand(name) {
 
         var front = $("<div>");
         front.addClass("front");
+
+        var textDiv = $("<div>");
+        textDiv.addClass("textDiv");
+
         for (let i = 0; i < cards.name.length; i++) {
             if (cards.name[i] === name) {
                 expandDiv.css("background-image", 'url(' + cards.image[i] + ')');
+                textDiv.text(cards.text[i]);
             }
         }
 
@@ -102,19 +106,22 @@ function expand(name) {
         newBorder.addClass("newBorder");
         newBorder.appendTo(".expand-tile");
 
+        // var backButton = $("<div>");
+        // backButton.attr("id","backButton");
+        // backButton.text("Back");
+        // $(backButton).hide().appendTo(".container").fadeIn(500);
+
         $(".expand-tile").animate({
             opacity: '0.90',
-            width: '80vw',
-            
+            width: '100%',
+        }, function () {
+            $(textDiv).hide().appendTo(".container").fadeIn(500);
+
         });
 
     });
 
-
-
-
-
-    var backButton = $("<div>");
+    
 
     $(".card-grid").flip({
         axis: 'y',
